e0d31b8d0fb7eda558379094e2da60e06bdb3007
comptergeeks <F@rhan0108>
2022-10-05 15:34:55 -0700

    Added numbers + beginning to work on gameplay

--- src/com/company/GameBoard.java
+++ src/com/company/GameBoard.java
@@ -9,13 +9,28 @@ public class GameBoard {
     public GameBoard() {
         hasWon = false;
     }
-    public void newGame() {
-         board = new String[5][3];
-        for (int i = 0; i < 5; i++) {
-            for (int x = 0; x < 3; x++) {
-                if (i ==  1 || i == 3) {
-                    board[i][x] = "--+--";
-                } else {
+    public void createBoard() {
+        int count2 = 0;
+         board = new String[6][4];
+        for (int i = 0; i < 6; i++) {
+            int count = 0;
+            for (int x = 0; x < 4; x++) {
+                if (i == 0) {
+                    if(x == 0) {
+                        board[i][x] = " ";
+                    } else {
+                        board[i][x] = "  " + String.valueOf(count + 1) + "  ";
+                        count++;
+                    }
+                }
+                else if (x == 0 && i%2 != 0){
+                    board[i][x] = String.valueOf(count2 + 1);
+                    count2++;
+
+                } else if (i ==  2 || i == 4) {
+                    board[i][x] = "----";
+                }
+                else {
                     board[i][x] = "  -  ";
                 }
                 }
@@ -27,7 +42,14 @@ public class GameBoard {
         Scanner sc = new Scanner(System.in);
         players = new String[2];
         for (int i = 0; i < 2; i++) {
-            System.out.println("What is Player " + (i + 1) + "'s name?");
+            String playerName = "What is Player " + (i + 1) + "'s name? ";
+            if(i == 0) {
+                playerName += "(X)";
+            }
+            if(i == 1) {
+                playerName += "(O)";
+            }
+            System.out.println(playerName);
             players[i] = sc.nextLine();
         }
         gameSession();
@@ -35,18 +57,36 @@ public class GameBoard {
     public void gameSession() {
         System.out.println("Your Board:");
         boardPrinter();
+        int count = 0;
+        while(hasWon == false) {
+            System.out.println(players[count] + "turn. " + "In put your value in the following format: row column");
+            Scanner sc = new Scanner(System.in);
+            sc.nextLine();
+
+        }
+
     }
 
 
 
 
     public void boardPrinter() {
-        for (int row = 0; row < 5; row++) {
-            for (int column = 0; column < 3; column++) {
+        for (int row = 0; row < 6; row++) {
+            for (int column = 0; column < 4; column++) {
                 System.out.print(board[row][column]);
             }
             System.out.println();
         }
+    }
+    public void checkInput() {
+
+    }
+    public void checkValue() {
+        //.contains(-)
+
+    }
+    public void checkGame() {
+
     }
 
 }
