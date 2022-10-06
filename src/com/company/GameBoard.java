09b1679000a1e36abdea617a44b12c5468bb24d8
comptergeeks <F@rhan0108>
2022-10-05 19:59:24 -0700

    Gameplay now working, finishing final win conditions.

--- src/com/company/GameBoard.java
+++ src/com/company/GameBoard.java
@@ -6,6 +6,8 @@ public class GameBoard {
     boolean hasWon;
     String[] players;
     String[][] board;
+    boolean player1;
+    boolean player2;
     public GameBoard() {
         hasWon = false;
     }
@@ -58,11 +60,28 @@ public class GameBoard {
         System.out.println("Your Board:");
         boardPrinter();
         int count = 0;
-        while(hasWon == false) {
-            System.out.println(players[count] + "turn. " + "In put your value in the following format: row column");
-            Scanner sc = new Scanner(System.in);
-            sc.nextLine();
-
+        player1 = true;
+        while(!hasWon) {
+            if(player1) {
+                count = 0;
+                System.out.println(players[count] + "'s turn. " + "In put your value in the following format: row column");
+                Scanner sc = new Scanner(System.in);
+                String input = sc.nextLine();
+                checkInput(input);
+                boardPrinter();
+                checkGame();
+                //count++;
+            } if (player2) {
+                count = 1;
+                System.out.println(players[count] + "'s turn. " + "In put your value in the following format: row column");
+                Scanner sc = new Scanner(System.in);
+                String input = sc.nextLine();
+                checkInput(input);
+                boardPrinter();
+                checkGame();
+                //count++;
+            }
+            //count = 0;
         }
 
     }
@@ -78,15 +97,72 @@ public class GameBoard {
             System.out.println();
         }
     }
-    public void checkInput() {
-
+    public void checkInput(String s) {
+        Scanner sc = new Scanner(s);
+        int row = 0;
+        int col = 0;
+        int[] inputArr = new int[2];
+        int howManyValues = 0;
+        int range = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (sc.hasNextInt()) {
+                howManyValues++;
+               int temp = sc.nextInt();
+               if (temp > 3|| temp < 1 ) {
+                   range++;
+               }
+            }
+        }
+        Scanner s2 = new Scanner(s);
+        if (howManyValues == 2 && range == 0) {
+            int count = 0;
+            for(int i = 0; i < s.length(); i++) {
+                if (s2.hasNextInt()) {
+                    inputArr[count] = s2.nextInt();
+                    count++;
+                }
+            }
+            checkValue(inputArr);
+        } else if(howManyValues == 0) {
+            System.out.println("You have entered no numbers, try again.");
+        } else if(range != 0) {
+            System.out.println("Your numbers are out of the range, try again.");
+        } else if(howManyValues > 2) {
+            System.out.println("You have entered too many values, try again.");
+        }
+        else {
+            System.out.println("An unknown error has occurred.");
+        }
     }
-    public void checkValue() {
+    public void checkValue(int[] arr) {
         //.contains(-)
-
-    }
+        for (int i = 0; i < arr.length; i++) {
+            if(i == 0 && arr[i] == 2) {
+                arr[i] = 3;
+            } else if (i == 0 && arr[i] == 3) {
+                arr[i] = 5;
+            }
+        }
+        if (board[arr[0]][arr[1]].contains("-")) {
+            if (player1) {
+                board[arr[0]][arr[1]] = "  X  ";
+                player1 = false;
+                player2 = true;
+            } else {
+                board[arr[0]][arr[1]] = "  O  ";
+                player2 = false;
+                player1 = true;
+            }
+        } else {
+            System.out.println("This value has already been placed, try again.");
+        }
+        }
     public void checkGame() {
+        //check if any row or column has three x's
+        for(int row = 0; row < 6; row++) {
+            for (int col = 0; col < 4; col++) {
 
+            }
+        }
     }
-
 }
